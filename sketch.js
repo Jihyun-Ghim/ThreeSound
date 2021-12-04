fde408364be771d8d308890b7f3156ad9eff61d1
Jihyun-Ghim <84126943+Jihyun-Ghim@users.noreply.github.com>
2021-12-05 01:52:58 +0900

    사운드 불러오고, 자동재생시키기

--- sketch.js
+++ sketch.js
@@ -1,12 +1,22 @@
-var innerSound;
-var movingSound;
-var dailySound;
+var test;
+
+// var innerSound;
+// var movingSound;
+// var dailySound;
+
+function preload() {
+  testSound = loadSound("testSound.mp3");
+
+  // innerSound = loadSound("innerSound.mp3");
+  // movingSound = loadSound("movingSound.mp3");
+  // dailySound = loadSound("dailySound.mp3");
+}
 
 function setup() {
   createCanvas(400, 400);
-  innerSound = loadSound("innerSound.mp3");
-  movingSound = loadSound("movingSound.mp3");
-  dailySound = loadSound("dailySound.mp3");
+  testSound.play();
+
+  // innerSound.play();
 }
 
 function draw() {

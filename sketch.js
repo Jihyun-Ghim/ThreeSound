79d2a76edf1ac8347112d6c69bcfd78a2885303a
Jihyun-Ghim <84126943+Jihyun-Ghim@users.noreply.github.com>
2021-12-05 03:56:43 +0900

    create volume Line

--- sketch.js
+++ sketch.js
@@ -2,15 +2,21 @@ var test;
 var sliderVolume;
 var sliderRate;
 var sliderPan;
+var amp;
+var volHistory = [];
 
 function setup() {
   createCanvas(400, 400);
 
   testSound = loadSound("testSound.mp3", loaded);
-
+  
   sliderVolume = createSlider(0, 1, 0.5, 0.01);
   sliderRate = createSlider(0, 2, 1, 0.01);
   sliderPan = createSlider(-1, 1, 0, 0.01);
+
+  amp = new p5.Amplitude();
+
+  background(0);
 }
 
 function loaded() {
@@ -18,8 +24,23 @@ function loaded() {
 }
 
 function draw() {
-  background(220);
   testSound.setVolume(sliderVolume.value());
   testSound.rate(sliderRate.value());
   testSound.pan(sliderPan.value());
+
+  var vol = amp.getLevel();
+  volHistory.push(vol);
+  stroke(255);
+  noFill();
+  translate(0, - height/2);
+  beginShape();
+  for (var i = 0; i < volHistory.length; i++){
+    var y = map(volHistory[i], 0, 1, height, 0);
+    vertex(i, y);
+  }
+  endShape(); //볼륨에 따라 선 그리기
+
+  if (volHistory.lenght > width) {
+    volHistory.splice(0, 1);
+  } //길이 지나면 기존에 있던 거 없애고 새로 시작하기
 }

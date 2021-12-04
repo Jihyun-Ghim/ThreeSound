f2034e0c5884e9cee998da501f730271d97071c3
Jihyun-Ghim <84126943+Jihyun-Ghim@users.noreply.github.com>
2021-12-05 02:21:01 +0900

    add slider

--- sketch.js
+++ sketch.js
@@ -1,24 +1,34 @@
 var test;
+var sliderVolume;
+var sliderRate;
+var sliderPan;
 
 // var innerSound;
 // var movingSound;
 // var dailySound;
 
-function preload() {
-  testSound = loadSound("testSound.mp3");
-
-  // innerSound = loadSound("innerSound.mp3");
-  // movingSound = loadSound("movingSound.mp3");
-  // dailySound = loadSound("dailySound.mp3");
-}
-
 function setup() {
   createCanvas(400, 400);
-  testSound.play();
 
-  // innerSound.play();
+  testSound = loadSound("testSound.mp3", loaded);
+
+  sliderVolume = createSlider(0, 1, 0.5, 0.01);
+  // slider.position(0, height);
+
+  sliderRate = createSlider(0, 2, 1, 0.01);
+  // slider.position(width-260, height);
+
+  sliderPan = createSlider(-1, 1, 0, 0.01);
+  // slider.position(width, height);
+}
+
+function loaded() {
+  testSound.play();
 }
 
 function draw() {
   background(220);
+  testSound.setVolume(sliderVolume.value());
+  testSound.rate(sliderRate.value());
+  testSound.pan(sliderPan.value());
 }

cb2402beaad8968f16631a013571f9ef0caab0aa
Jihyun-Ghim <84126943+Jihyun-Ghim@users.noreply.github.com>
2021-12-05 03:12:59 +0900

    add slider

--- sketch.js
+++ sketch.js
@@ -3,23 +3,14 @@ var sliderVolume;
 var sliderRate;
 var sliderPan;
 
-// var innerSound;
-// var movingSound;
-// var dailySound;
-
 function setup() {
   createCanvas(400, 400);
 
   testSound = loadSound("testSound.mp3", loaded);
 
   sliderVolume = createSlider(0, 1, 0.5, 0.01);
-  // slider.position(0, height);
-
   sliderRate = createSlider(0, 2, 1, 0.01);
-  // slider.position(width-260, height);
-
   sliderPan = createSlider(-1, 1, 0, 0.01);
-  // slider.position(width, height);
 }
 
 function loaded() {
